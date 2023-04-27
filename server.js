8b5025bcac75e7704b5fb98090c152d925c9774e
kayisha Ulysse <kayishaulysse@kayishas-MBP.fios-router.home>
2023-04-26 22:07:51 -0400

    Server.js edit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const mongoose = require ('mongoose');
 const app = express ();
 const db = mongoose.connection;
-// const Carbon = require('./models/carbon.js')
+const Topic = require('./models/topic.js')
 const cors =require('cors')
 require('dotenv').config()
 app.use(cors())
